ae69997bb147732ebfc76450e727d193ea118c83
nataliawcislo <49069886+nataliawcislo@users.noreply.github.com>
2023-06-04 20:58:57 +0200

    Update backgroud.swift

--- backgroud.swift
+++ backgroud.swift
@@ -14,11 +14,9 @@ class Grandparent: Person {
     var childrenList: [Parent] = []
     var grandchildrenList: [Child]  = []
     
-    init(childrenList:[Parent], grandchildrenList: [Child] ) {
+    override init(name: String, height: Double, birthday: String) {
         super.init(name: name, height: height, birthday: birthday)
-        self.childrenList = childrenList    
-        self.grandchildrenList = grandchildrenList    
-    }
+     }
     
     func printGrandchildenList(){
         print(childrenList)  
@@ -37,23 +35,21 @@ class Grandparent: Person {
     }
 }
 
-class Parent: Grandparent {
-     init() {
-          super.init(childrenList: childrenList, grandchildrenList: grandchildrenList)
+class Parent: Person {
+    override init(name: String, height: Double, birthday: String) {
+        super.init(name: name, height: height, birthday: birthday)
      }
 }
 
 
-class Child: Parent {
+class Child: Person {
     var grandparentsList = [Grandparent]()
     var parentsList = [Parent]()
    
-    init(grandparentsList:[Grandparent], parentsList: [Parent]) {
-        super.init()
-        self.grandparentsList = grandparentsList
-        self.parentsList = parentsList
+     override init(name: String, height: Double, birthday: String) {
+        super.init(name: name, height: height, birthday: birthday)
      }
-       
+
     func printGrandparentsList(){
         print(grandparentsList)     
     }
@@ -70,3 +66,16 @@ class Child: Parent {
          grandparentsList.append(grandparent)
      }
 }
+
+var c1 = Child(name: "N", height: 160.9, birthday: "30-08-1998")
+var c2 = Child(name: "B", height: 160.9, birthday: "30-08-1998")
+var c3 = Child(name: "C", height: 160.9, birthday: "30-08-1998")
+var p1 = Parent(name: "G", height: 160.9, birthday: "30-08-1998")
+var p2 = Parent(name: "7", height: 160.9, birthday: "30-08-1998")
+var g1 = Grandparent(name: "Nati", height: 160.9, birthday: "30-08-1998")
+print("\(c1.name), \(c1.height)")
+print("\(c2.name), \(c2.height)")
+print("\(c3.name), \(c3.height)")
+//Grandparent.addGrandchild(c1)
+//Grandparent.addGrandchild(c2)
+//Grandparent.addGrandchild(c3)
